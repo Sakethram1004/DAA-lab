3092f9ee14ca86e71ba87d00949c7c995eaeb570
katakamsakethram <80837734+Sakethram1004@users.noreply.github.com>
2023-12-07 12:11:50 +0530

    6.Given a bunch of projects(job).py

--- 6.Given a bunch of projects(job).py
+++ 6.Given a bunch of projects(job).py
@@ -1,33 +1,22 @@
 class Job:
     def __init__(self, profit, deadline, q_no):
-        self.profit = profit
-        self.deadline = deadline
-        self.q = q_no
+        self.profit, self.deadline, self.q = profit, deadline, q_no
 
 def merge(l, low, high, mid):
-    i = low
-    j = mid + 1
-    temp = []
-    while (i <= mid and j <= high):
-        if (l[i].profit > l[j].profit):
-            temp.append(l[i])
-            i += 1
-        else:
-            temp.append(l[j])
-            j += 1
-    while (i <= mid):
-        temp.append(l[i])
-        i += 1
-    while (j <= high):
-        temp.append(l[j])
-        j += 1
-    for i in range(len(temp)):
-        l[low + i] = temp[i]
+    i, j, temp = low, mid + 1, []
+    while i <= mid and j <= high:
+        temp.append(l[i] if l[i].profit > l[j].profit else l[j])
+        i += 1 if l[i].profit > l[j].profit else 0
+        j += 1 if l[j].profit > l[i].profit else 0
+    temp.extend(l[i:mid + 1])
+    temp.extend(l[j:high + 1])
+    for i, item in enumerate(temp):
+        l[low + i] = item
     return l
 
 def merge_sort(l, low, high):
     mid = (low + high) // 2
-    if (low < high):
+    if low < high:
         merge_sort(l, low, mid)
         merge_sort(l, mid + 1, high)
         merge(l, low, high, mid)
@@ -35,14 +24,9 @@ def merge_sort(l, low, high):
 
 def job_scheduling(l):
     l = merge_sort(l, 0, len(l) - 1)
-    max_deadline = 0
-    for i in range(len(l)):
-        if l[i].deadline > max_deadline:
-            max_deadline = l[i].deadline
-    
-    x = [None] * (max_deadline)
-    i = 0
-    k = 0
+    max_deadline = max((job.deadline for job in l), default=0)
+    x = [None] * max_deadline
+    i, k = 0, 0
     while k < len(l):
         ind = l[i].deadline - 1
         k += 1
@@ -50,29 +34,21 @@ def job_scheduling(l):
             x[ind] = l[i]
         else:
             count = ind
-            for j in range(count):
-                ind -= 1
-                if ind >= 0 and x[ind] is None:
-                    x[ind] = l[i]
+            for j in range(count, -1, -1):
+                if x[j] is None:
+                    x[j] = l[i]
                     break
         i += 1
     return x
 
-n = int(input("Enter the no of Jobs: "))
-qs = []
-print("Enter Pi: profit and Di: deadline for each Ji")
-for i in range(n):
-    profit, deadline = [int(x) for x in input().split()]
-    qs.append(Job(profit, deadline, i + 1))
+# Input
+n = int(input("Enter the number of Jobs: "))
+qs = [Job(*map(int, input("Enter profit, deadline, q_no: ").split())) for _ in range(n)]
 
+# Execution
 result = job_scheduling(qs)
-total = 0
-job_subset = []
+total, l = sum(job.profit for job in result if job), [job.q for job in result if job]
 
-for i in range(len(result)):
-    if result[i] is not None:
-        total += result[i].profit
-        job_subset.append(result[i].q)
-
-print("The Jobs subset is:", job_subset)
+# Output
+print("The Jobs subset is:", l)
 print("The max profit within deadline is:", total)
